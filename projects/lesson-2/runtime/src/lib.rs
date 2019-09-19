ddc4de6ebe1823e4b8f0a672933fd00e94f6a65b
k.f.gamehacker <k.f.gamehacker@gmail.com>
2019-09-19 19:40:31 +0800

    config kitties in the runtime lib file

--- projects/lesson-2/runtime/src/lib.rs
+++ projects/lesson-2/runtime/src/lib.rs
@@ -64,6 +64,9 @@ pub type DigestItem = generic::DigestItem<Hash>;
 /// Used for the module template in `./template.rs`
 mod template;
 
+// add crypto kitties module
+mod cryptokitties;
+
 /// Opaque types. These are used by the CLI to instantiate machinery that don't need to know
 /// the specifics of the runtime. They can then be made to be agnostic over specific formats
 /// of data like extrinsics, allowing for them to continue syncing the network through upgrades
@@ -256,6 +259,11 @@ impl template::Trait for Runtime {
 	type Event = Event;
 }
 
+// add crypto kitties trait
+impl cryptokitties::Trait for Runtime{
+	type Event = Event;
+}
+
 construct_runtime!(
 	pub enum Runtime where
 		Block = Block,
@@ -269,8 +277,10 @@ construct_runtime!(
 		Indices: indices::{default, Config<T>},
 		Balances: balances::{default, Error},
 		Sudo: sudo,
-		// Used for the module template in `./template.rs`
+		// Used for the module template in `./template.rs``
 		TemplateModule: template::{Module, Call, Storage, Event<T>},
+
+		Cryptokitties: cryptokitties::{Module,Call,Storage,Event<T>},
 	}
 );
 
