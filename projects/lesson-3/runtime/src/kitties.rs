4e3499daffb0b3a1a2fffd382e16226a1f2ec894
anray <anray1980@126.com>
2019-09-22 19:30:51 +0800

    第三次作业

--- projects/lesson-3/runtime/src/kitties.rs
+++ projects/lesson-3/runtime/src/kitties.rs
@@ -3,10 +3,9 @@ use codec::{Encode, Decode};
 use runtime_io::blake2_128;
 use system::ensure_signed;
 
-pub trait Trait: system::Trait {
-}
+pub trait Trait: system::Trait {}
 
-#[derive(Encode, Decode, Default)]
+#[derive(Encode, Decode, Default, Clone)]
 pub struct Kitty(pub [u8; 16]);
 
 decl_storage! {
@@ -24,14 +23,37 @@ decl_module! {
 		pub fn create(origin) {
 			let sender = ensure_signed(origin)?;
 			let count = Self::kitties_count();
-			if count == u32::max_value() {
-				return Err("Kitties count overflow");
-			}
-			let payload = (<system::Module<T>>::random_seed(), sender, <system::Module<T>>::extrinsic_index(), <system::Module<T>>::block_number());
+			let new_count = count.checked_add(1).ok_or("Kitties count overflow")?;
+
+			let payload = (<system::Module<T>>::random_seed(), sender,
+			    <system::Module<T>>::extrinsic_index(), <system::Module<T>>::block_number());
 			let dna = payload.using_encoded(blake2_128);
 			let kitty = Kitty(dna);
 			Kitties::insert(count, kitty);
-			KittiesCount::put(count + 1);
+			KittiesCount::put(new_count);
 		}
+
+        /// Breed a kitty
+        pub fn breed(origin, dna1: u32, dna2: u32) {
+   			let sender = ensure_signed(origin)?;
+   			let count = Self::kitties_count();
+			let new_count = count.checked_add(1).ok_or("Kitties count overflow")?;
+
+            let kitty_1 = Self::kitty(dna1);
+            let kitty_2 = Self::kitty(dna2);
+
+            let payload = (<system::Module<T>>::random_seed(), sender,
+			    <system::Module<T>>::extrinsic_index(), <system::Module<T>>::block_number());
+
+            let mut final_dna = kitty_1.0;
+            for (i, (dna_2_element, r)) in kitty_2.0.using_encoded(blake2_128).iter().zip(payload.using_encoded(blake2_128).iter()).enumerate() {
+                if r % 2 == 0 {
+                    final_dna.as_mut()[i] = *dna_2_element;
+                }
+            }
+
+            Kitties::insert(count, Kitty(final_dna));
+			KittiesCount::put(new_count);
+        }
 	}
 }
