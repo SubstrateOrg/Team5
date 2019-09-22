26be676aa2d9cce9e8ac5f6a3e2622fda089f072
k.f.gamehacker <k.f.gamehacker@gmail.com>
2019-09-22 22:23:18 +0800

    implement the breeding function

--- projects/lesson-3/runtime/src/kitties.rs
+++ projects/lesson-3/runtime/src/kitties.rs
@@ -6,8 +6,17 @@ use system::ensure_signed;
 pub trait Trait: system::Trait {
 }
 
-#[derive(Encode, Decode, Default)]
-pub struct Kitty(pub [u8; 16]);
+// #[derive(Encode, Decode, Default)]
+// pub struct Kitty(pub [u8; 16]);
+
+//using my kitty struct instead
+#[derive(Debug, Encode, Decode, Default, Clone, PartialEq)]
+pub struct Kitty<Hash,Balance>{
+    id:Hash,
+    dna:u128,
+    price:Balance,
+    gen:u64,
+}
 
 decl_storage! {
 	trait Store for Module<T: Trait> as Kitties {
@@ -32,6 +41,41 @@ decl_module! {
 			let kitty = Kitty(dna);
 			Kitties::insert(count, kitty);
 			KittiesCount::put(count + 1);
+		}
+
+		//breed a kitty from parent
+		pub fn breed_kitty(origin,father_kitty_hash: T::Hash, mother_kitty_hash: T::Hash){
+			
+			// checking msg sender
+			let sender = ensure_signed(origin)?;
+
+			// parents hash must exist
+			ensure!(<Kittyies<T>>::exists(father_kitty_hash));
+			ensure!(<Kittyies<T>>::exists(mother_kitty_hash));
+
+			// get parents struct
+			let father_kitty = Self::kitty(father_kitty_hash);
+			let mother_kitty_hash = Self::kitty(father_kitty_hash);
+
+			// todo: mix parents DNA to generate child new DNA
+			let child_dna = 0;
+			
+			// construct the child kitty
+			let child_kitty = Kitty {
+                id: random_hash,
+                dna: child_dna,
+                price: <T::Balance as As<u64>>::sa(0),
+                gen: cmp::max(kitty_1.gen, kitty_2.gen) + 1,
+            };
+
+			// checking before inserting 
+			ensure!(!<Kitties<T>>::exists(child_kitty.id), "Kitty already exists");
+
+			// add the new child kitty to the data storage
+			Kitties::insert(count, kitty);
+			KittiesCount::put(count + 1);
+
+			ok(())
 		}
 	}
 }
