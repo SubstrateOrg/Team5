607eee100a26eddf9c180c68c78f87866971c330
sunqiLoveSnow <qi.sun@cybex.io>
2019-09-23 15:02:20 +0800

    add storage and api to track kitty owner

--- projects/lesson-3/runtime/src/kitties.rs
+++ projects/lesson-3/runtime/src/kitties.rs
@@ -12,9 +12,13 @@ pub struct Kitty(pub [u8; 16]);
 decl_storage! {
 	trait Store for Module<T: Trait> as Kitties {
 		/// Stores all the kitties, key is the kitty id / index
-		pub Kitties get(kitty): map u32 => Kitty;
+		pub Kitties get(kitty): map u32 => Option<Kitty>;
+		/// Stores owner of kitties, key is the kitty id / index, value is kitty's owner
+        pub KittyOwner get(owner_of): map u32 => Option<T::AccountId>;
 		/// Stores the total number of kitties. i.e. the next kitty index
 		pub KittiesCount get(kitties_count): u32;
+		/// Stores the number of kitties under a owner. i.e. the next kitty index
+        pub OwnedKittiesCount get(owned_kitty_count): map T::AccountId => u32;
 	}
 }
 
@@ -27,19 +31,29 @@ decl_module! {
 			if count == u32::max_value() {
 				return Err("Kitties count overflow");
 			}
-			let payload = (<system::Module<T>>::random_seed(), sender, <system::Module<T>>::extrinsic_index(), <system::Module<T>>::block_number());
+			let own_count = Self::owned_kitty_count(&sender);
+            if own_count == u32::max_value() {
+				return Err("OwnKitties count overflow");
+			}
+	
+			let payload = (<system::Module<T>>::random_seed(), &sender, <system::Module<T>>::extrinsic_index(), <system::Module<T>>::block_number());
 			let dna = payload.using_encoded(blake2_128);
 			let kitty = Kitty(dna);
 			Kitties::insert(count, kitty);
 			KittiesCount::put(count + 1);
+            <KittyOwner<T>>::insert(count, sender.clone());
+            <OwnedKittiesCount<T>>::insert(sender.clone(), own_count + 1 );
+
 			Ok(())
 		}
 		// Breed a new kitty
 		pub fn breed(origin, father: u32, mother: u32) -> Result{
 			let sender = ensure_signed(origin)?;
 			let count = Self::kitties_count();
+			let own_count = Self::owned_kitty_count(&sender);
 			
 			let new_count = count.checked_add(1).ok_or("Overflow adding a new kitty to total supply")?;
+			let new_own_count = own_count.checked_add(1).ok_or("Overflow adding a new kitty to owner supply")?;
 
 			ensure!(Kitties::exists(father), "This father does not exist");
             ensure!(Kitties::exists(mother), "This mother does not exist");
@@ -49,6 +63,8 @@ decl_module! {
 
             let kitty_father = Self::kitty(father);
             let kitty_mother = Self::kitty(mother);
+            let kitty_father = kitty_father.unwrap();
+            let kitty_mother = kitty_mother.unwrap();
 
             let mut final_dna = kitty_father.0;
             for (i, (dna_2_element, r)) in kitty_mother.0.iter().zip(random_hash.iter()).enumerate() {
@@ -61,6 +77,10 @@ decl_module! {
 
 			Kitties::insert(count, kitty);
 			KittiesCount::put(new_count);
+            <KittyOwner<T>>::insert(count, sender.clone());
+            <OwnedKittiesCount<T>>::insert(sender.clone(), new_own_count );
+
+
 			Ok(())
 		}
 	}
