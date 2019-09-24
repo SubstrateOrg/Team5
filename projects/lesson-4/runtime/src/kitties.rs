2760820a407be4b7ca22604354a4f564c8c0113a
sunqiLoveSnow <qi.sun@cybex.io>
2019-09-24 15:42:25 +0800

    lesson4 finished

--- projects/lesson-4/runtime/src/kitties.rs
+++ projects/lesson-4/runtime/src/kitties.rs
@@ -1,12 +1,12 @@
 use support::{decl_module, decl_storage, ensure, StorageValue, StorageMap, dispatch::Result, Parameter};
-use sr_primitives::traits::{SimpleArithmetic, Bounded};
+use sr_primitives::traits::{SimpleArithmetic, Bounded, CheckedAdd, CheckedSub};
 use codec::{Encode, Decode};
 use runtime_io::blake2_128;
 use system::ensure_signed;
 use rstd::result;
 
 pub trait Trait: system::Trait {
-	type KittyIndex: Parameter + SimpleArithmetic + Bounded + Default + Copy;
+	type KittyIndex: Parameter + SimpleArithmetic + Bounded + Default + Copy ;
 }
 
 #[derive(Encode, Decode)]
@@ -19,46 +19,40 @@ decl_storage! {
 		/// Stores the total number of kitties. i.e. the next kitty index
 		pub KittiesCount get(kitties_count): T::KittyIndex;
 
+		/// get owner by global kitty Index
+		pub KittyOwner get(owner_of): map T::KittyIndex => Option<T::AccountId>;
+
 		/// Get kitty ID by account ID and user kitty index
 		pub OwnedKitties get(owned_kitties): map (T::AccountId, T::KittyIndex) => T::KittyIndex;
 		/// Get number of kitties by account ID
 		pub OwnedKittiesCount get(owned_kitties_count): map T::AccountId => T::KittyIndex;
+		/// get owner kitty index by global kitty index
+		pub OwnedKittiesIndex: map T::KittyIndex => T::KittyIndex ;
 	}
 }
 
 decl_module! {
 	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
 		/// Create a new kitty
-		pub fn create(origin) {
+		pub fn create(origin) -> Result {
 			let sender = ensure_signed(origin)?;
-
 			// 作业：重构create方法，避免重复代码
-
-			let kitty_id = Self::kitties_count();
-			if kitty_id == T::KittyIndex::max_value() {
-				return Err("Kitties count overflow");
-			}
-
-			// Generate a random 128bit value
-			let payload = (<system::Module<T>>::random_seed(), &sender, <system::Module<T>>::extrinsic_index(), <system::Module<T>>::block_number());
-			let dna = payload.using_encoded(blake2_128);
-
-			// Create and store kitty
-			let kitty = Kitty(dna);
-			<Kitties<T>>::insert(kitty_id, kitty);
-			<KittiesCount<T>>::put(kitty_id + 1.into());
-
-			// Store the ownership information
-			let user_kitties_id = Self::owned_kitties_count(&sender);
-			<OwnedKitties<T>>::insert((sender.clone(), user_kitties_id), kitty_id);
-			<OwnedKittiesCount<T>>::insert(sender, user_kitties_id + 1.into());
+			Self::do_create(sender)
 		}
 
 		/// Breed kitties
-		pub fn breed(origin, kitty_id_1: T::KittyIndex, kitty_id_2: T::KittyIndex) {
+		pub fn breed(origin, kitty_id_1: T::KittyIndex, kitty_id_2: T::KittyIndex) ->Result {
 			let sender = ensure_signed(origin)?;
 
-			Self::do_breed(sender, kitty_id_1, kitty_id_2)?;
+			Self::do_breed(sender, kitty_id_1, kitty_id_2)
+		}
+		pub fn transfer(origin, to: T::AccountId, kitty_id: T::KittyIndex) -> Result{
+			let sender = ensure_signed(origin)?;
+			let owner = Self::owner_of(kitty_id).ok_or("No owner for this kitty")?;
+            ensure!(owner == sender, "You do not own this kitty");
+
+			Self::do_transfer(sender, to, kitty_id)
+			// Ok(())
 		}
 	}
 }
@@ -69,12 +63,12 @@ fn combine_dna(dna1: u8, dna2: u8, selector: u8) -> u8 {
 	// selector.map_bits(|bit, index| if (bit == 1) { dna1 & (1 << index) } else { dna2 & (1 << index) })
 	// 注意 map_bits这个方法不存在。只要能达到同样效果，不局限算法
 	// 测试数据：dna1 = 0b11110000, dna2 = 0b11001100, selector = 0b10101010, 返回值 0b11100100
-	return dna1;
+	(dna1 & selector) | (dna2 & (!selector))
 }
 
 impl<T: Trait> Module<T> {
 	fn random_value(sender: &T::AccountId) -> [u8; 16] {
-		let payload = (<system::Module<T>>::random_seed(), sender, <system::Module<T>>::extrinsic_index(), <system::Module<T>>::block_number());
+		let payload = (<system::Module<T>>::random_seed(), &sender, <system::Module<T>>::extrinsic_index(), <system::Module<T>>::block_number());
 		payload.using_encoded(blake2_128)
 	}
 
@@ -94,7 +88,49 @@ impl<T: Trait> Module<T> {
 		// Store the ownership information
 		let user_kitties_id = Self::owned_kitties_count(owner.clone());
 		<OwnedKitties<T>>::insert((owner.clone(), user_kitties_id), kitty_id);
-		<OwnedKittiesCount<T>>::insert(owner, user_kitties_id + 1.into());
+		<OwnedKittiesCount<T>>::insert(owner.clone(), user_kitties_id + 1.into());
+		
+		<OwnedKittiesIndex<T>>::insert(kitty_id, user_kitties_id);
+		<KittyOwner<T>>::insert(kitty_id, owner.clone());
+	}
+	fn do_transfer(from: T::AccountId, to : T::AccountId, kitty_id: T::KittyIndex) -> Result {
+		let owned_kitty_count_from = Self::owned_kitties_count(&from);
+        let owned_kitty_count_to = Self::owned_kitties_count(&to);
+
+        let new_owned_kitty_count_to = owned_kitty_count_to.checked_add(&1.into())
+            .ok_or("Transfer causes overflow of 'to' kitty ")?;
+
+        let new_owned_kitty_count_from = owned_kitty_count_from.checked_sub(&1.into())
+            .ok_or("Transfer causes underflow of 'from' kitty ")?;
+		let kitty_index = <OwnedKittiesIndex<T>>::get(kitty_id);
+
+        if kitty_index != new_owned_kitty_count_from {
+            let last_kitty_id = <OwnedKitties<T>>::get((from.clone(), new_owned_kitty_count_from));
+            <OwnedKitties<T>>::insert((from.clone(), kitty_index), last_kitty_id);
+            <OwnedKittiesIndex<T>>::insert(last_kitty_id, kitty_index);
+        }
+		<KittyOwner<T>>::insert(&kitty_id, &to);
+        <OwnedKittiesIndex<T>>::insert(kitty_id, owned_kitty_count_to);
+
+        <OwnedKitties<T>>::remove((from.clone(), new_owned_kitty_count_from));
+        <OwnedKitties<T>>::insert((to.clone(), owned_kitty_count_to), kitty_id);
+
+        <OwnedKittiesCount<T>>::insert(&from, new_owned_kitty_count_from);
+        <OwnedKittiesCount<T>>::insert(&to, new_owned_kitty_count_to);
+
+		Ok(())
+	}
+	fn do_create(owner: T::AccountId) -> Result {
+		let kitty_id = Self::next_kitty_id()?;
+
+		// Generate a random 128bit value
+		let dna = Self::random_value(&owner);
+
+		// Create and store kitty
+		
+		let kitty = Kitty(dna);
+		Self::insert_kitty(owner.clone(), kitty_id, kitty );
+		Ok(())
 	}
 
 	fn do_breed(sender: T::AccountId, kitty_id_1: T::KittyIndex, kitty_id_2: T::KittyIndex) -> Result {
@@ -122,5 +158,77 @@ impl<T: Trait> Module<T> {
 		Self::insert_kitty(sender, kitty_id, Kitty(new_dna));
 
 		Ok(())
+	}
+}
+
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	use runtime_io::with_externalities;
+	use primitives::{H256, Blake2Hasher};
+	use support::{impl_outer_origin, assert_ok, parameter_types};
+	use sr_primitives::{traits::{BlakeTwo256, IdentityLookup}, testing::Header};
+	use sr_primitives::weights::Weight;
+	use sr_primitives::Perbill;
+
+	impl_outer_origin! {
+		pub enum Origin for Test {}
+	}
+
+	// For testing the module, we construct most of a mock runtime. This means
+	// first constructing a configuration type (`Test`) which `impl`s each of the
+	// configuration traits of modules we want to use.
+	#[derive(Clone, Eq, PartialEq)]
+	pub struct Test;
+	parameter_types! {
+		pub const BlockHashCount: u64 = 250;
+		pub const MaximumBlockWeight: Weight = 1024;
+		pub const MaximumBlockLength: u32 = 2 * 1024;
+		pub const AvailableBlockRatio: Perbill = Perbill::from_percent(75);
+	}
+	impl system::Trait for Test {
+		type Origin = Origin;
+		type Call = ();
+		type Index = u64;
+		type BlockNumber = u64;
+		type Hash = H256;
+		type Hashing = BlakeTwo256;
+		type AccountId = u64;
+		type Lookup = IdentityLookup<Self::AccountId>;
+		type Header = Header;
+		type WeightMultiplierUpdate = ();
+		type Event = ();
+		type BlockHashCount = BlockHashCount;
+		type MaximumBlockWeight = MaximumBlockWeight;
+		type MaximumBlockLength = MaximumBlockLength;
+		type AvailableBlockRatio = AvailableBlockRatio;
+		type Version = ();
+	}
+	impl Trait for Test {
+		type KittyIndex = u32;
+	}
+	type TestModule = Module<Test>;
+
+	// This function basically just builds a genesis storage key/value store according to
+	// our desired mockup.
+	fn new_test_ext() -> runtime_io::TestExternalities<Blake2Hasher> {
+		system::GenesisConfig::default().build_storage::<Test>().unwrap().into()
+	}
+
+	#[test]
+	fn test_dna_combine() {
+		with_externalities(&mut new_test_ext(), || {
+			// Just a dummy test for the dummy funtion `do_something`
+			// calling the `do_something` function with a value 42
+			// assert_ok!(TestModule::do_something(Origin::signed(1), 42));
+			// 测试数据：dna1 = 0b11110000, dna2 = 0b11001100, selector = 0b10101010, 返回值 0b11100100
+			let father:u8 = 0b11110000;
+			let mother:u8 = 0b11001100;
+			let selector:u8 = 0b10101010;
+			assert_eq!(combine_dna(father, mother, selector), 0b11100100) ;
+			
+		});
 	}
 }
