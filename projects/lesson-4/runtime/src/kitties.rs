f5a8e8c31eb45d50d885f7b725f96698de24959d
gobuddy <shawn.lisheng@outlook.com>
2019-09-26 18:57:23 +0800

    features for lesson 4

--- projects/lesson-4/runtime/src/kitties.rs
+++ projects/lesson-4/runtime/src/kitties.rs
@@ -26,7 +26,7 @@ decl_storage! {
 	}
 }
 
-decl_module! {
+decl_module! {	
 	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
 		/// Create a new kitty
 		pub fn create(origin) {
@@ -34,24 +34,14 @@ decl_module! {
 
 			// 作业：重构create方法，避免重复代码
 
-			let kitty_id = Self::kitties_count();
-			if kitty_id == T::KittyIndex::max_value() {
-				return Err("Kitties count overflow");
-			}
+			let kitty_id = Self::next_kitty_id()?;
 
 			// Generate a random 128bit value
-			let payload = (<system::Module<T>>::random_seed(), &sender, <system::Module<T>>::extrinsic_index(), <system::Module<T>>::block_number());
-			let dna = payload.using_encoded(blake2_128);
+			let dna = Self::random_value(&sender);
 
 			// Create and store kitty
 			let kitty = Kitty(dna);
-			<Kitties<T>>::insert(kitty_id, kitty);
-			<KittiesCount<T>>::put(kitty_id + 1.into());
-
-			// Store the ownership information
-			let user_kitties_id = Self::owned_kitties_count(&sender);
-			<OwnedKitties<T>>::insert((sender.clone(), user_kitties_id), kitty_id);
-			<OwnedKittiesCount<T>>::insert(sender, user_kitties_id + 1.into());
+			Self::insert_kitty(sender, kitty_id, kitty);
 		}
 
 		/// Breed kitties
@@ -69,7 +59,7 @@ fn combine_dna(dna1: u8, dna2: u8, selector: u8) -> u8 {
 	// selector.map_bits(|bit, index| if (bit == 1) { dna1 & (1 << index) } else { dna2 & (1 << index) })
 	// 注意 map_bits这个方法不存在。只要能达到同样效果，不局限算法
 	// 测试数据：dna1 = 0b11110000, dna2 = 0b11001100, selector = 0b10101010, 返回值 0b11100100
-	return dna1;
+	((selector & dna1) | (!selector & dna2))
 }
 
 impl<T: Trait> Module<T> {
@@ -122,5 +112,11 @@ impl<T: Trait> Module<T> {
 		Self::insert_kitty(sender, kitty_id, Kitty(new_dna));
 
 		Ok(())
+	}
+
+    //转移猫
+	fn transfer(from: T::AccountId, to: T::AccountId, kitty_id: T::KittyIndex)  {
+		<OwnedKitties<T>>::remove((from.clone(), kitty_id));
+
 	}
 }
