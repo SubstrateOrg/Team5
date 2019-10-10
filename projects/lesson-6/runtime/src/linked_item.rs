f23fe759a15ec19a446595190c5d04ddd5a1b753
k.f.gamehacker <k.f.gamehacker@gmail.com>
2019-10-10 19:30:13 +0800

    implement append & remove

--- projects/lesson-6/runtime/src/linked_item.rs
+++ projects/lesson-6/runtime/src/linked_item.rs
@@ -4,6 +4,7 @@ use codec::{Encode, Decode};
 
 #[cfg_attr(feature = "std", derive(Debug, PartialEq, Eq))]
 #[derive(Encode, Decode)]
+// double direction linked list
 pub struct LinkedItem<Value> {
 	pub prev: Option<Value>,
 	pub next: Option<Value>,
@@ -37,9 +38,56 @@ impl<Storage, Key, Value> LinkedList<Storage, Key, Value> where
 
     pub fn append(key: &Key, value: Value) {
         // 作业：实现 append
+
+		// update head to reservely traversing the linked list
+		let current_head = Self::read_head(key);
+		let new_head = LinkedItem{
+			prev: Some(value),
+			next: current_head.next,
+		};
+
+		// write new head
+		Self::write(key, new_head);
+
+		// get the rear item
+		let rear_item = Self::read(key,head.prev);
+
+		let new_rear_item = LinkedItem{
+			prev: rear_item.prev,
+			next: Some(value),
+		};
+		
+		// set a new rear
+		Self::write(key,head.prev,new_rear_item);
+
+		let new_item = LinkedItem {
+			prev: head.prev,
+			next: None,
+		}
+
+		Self::write(key, Some(value), new_item);
     }
 
     pub fn remove(key: &Key, value: Value) {
         // 作业：实现 remove
+		if let Some(item) = Storage::take(&(key.clone(),Some(value))))){
+			
+			let prev = Self::read(key, item.prev);
+
+			let new_prev = LinkedItem{
+				prev: prev.prev,
+				next: item.next,
+			}
+
+			Self::write(key, item.prev, new_prev);
+
+			let next = Self::read(key, item.next);
+			let new_next = LinkedItem {
+				prev: item.prev,
+				next: next.next,
+			}
+
+			Self::write(key, item.next, new_next);
+		}
     }
 }
